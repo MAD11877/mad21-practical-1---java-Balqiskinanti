e110e2e6f22b035927a5bfbdc107f243733f6fc2
Balqiskinanti <S10204973@connect.np.edu.sg>
2021-04-23 03:18:36 +0000

    question5 done

--- Question5.java
+++ Question5.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Question5
 {
@@ -27,6 +28,31 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
-    
+    ArrayList<Integer> numList = new ArrayList<>();
+    int totalNum = in.nextInt();
+
+    for(int i=0; i<totalNum;i++){
+      System.out.print("Enter the numbers:");
+      int num = in.nextInt();
+      numList.add(num);
+    }
+
+
+    int getMode = numList.get(0);
+    int maxCount = 0;
+    for (int i = 0; i < numList.size(); i++) {
+        int value = numList.get(i);
+        int count = 1;
+        for (int u = 0; u < numList.size(); u++) {
+            if (numList.get(u) == value) {
+              count++;
+            }
+            if (count > maxCount) {
+                getMode = value;
+                maxCount = count; //update maxCount
+            }
+        }
+    }
+    System.out.print(getMode);
   }
 }
